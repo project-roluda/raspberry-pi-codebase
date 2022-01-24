cdc61e241ce6e1fe6ee4eb9d52fd5294f40be4e3
TheFloatingString <laurence.w.liang@gmail.com>
2022-01-24 15:57:28 -0500

    Add neutral position code

--- main.py
+++ main.py
@@ -10,17 +10,66 @@ GPIO.setup(7, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
-GPIO.setup(29, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 
-cm1 = GPIO.PWM(7, 15)
-cm2 = GPIO.PWM(11, 15)
-cm3 = GPIO.PWM(13, 15)
-cm4 = GPIO.PWM(15, 15)
+cm1 = GPIO.PWM(7, 50)
+cm2 = GPIO.PWM(11, 50)
+cm3 = GPIO.PWM(13, 50)
+cm4 = GPIO.PWM(15, 50)
+cm5 = GPIO.PWM(16, 50)
 
-for i in range(1):
-    throttle.throttle(cm1)
-    throttle.throttle(cm2)
-    throttle.throttle(cm3)
-    throttle.throttle(cm4)
+#for i in range(1):
+    #throttle.throttle(cm1, 20)
+#    throttle.throttle(cm2)
+#    throttle.throttle(cm3)
+#    throttle.throttle(cm4)
 
+#throttle.rotateAngle(cm1, 10)
+
+start = time.time()
+
+cm1.start(0)
+cm2.start(0)
+cm3.start(0)
+cm4.start(0)
+
+GPIO.output(7, True)
+GPIO.output(11, True)
+GPIO.output(13, True)
+GPIO.output(15, True)
+
+cm1.ChangeDutyCycle(9.5)
+cm2.ChangeDutyCycle(9.5)
+cm3.ChangeDutyCycle(9.5)
+cm4.ChangeDutyCycle(9.5)
+time.sleep(1)
+
+cm1.ChangeDutyCycle(3.5)
+cm2.ChangeDutyCycle(3.5)
+cm3.ChangeDutyCycle(3.5)
+cm4.ChangeDutyCycle(3.5)
+time.sleep(1)
+
+
+while (time.time()-start) < 5:
+    
+    cm1.ChangeDutyCycle(7)
+    cm2.ChangeDutyCycle(7)
+    cm3.ChangeDutyCycle(7)
+    cm4.ChangeDutyCycle(7)
+    
+    """
+    throttle.neutral_position(cm1, 7)
+    throttle.neutral_position(cm2, 11)
+    throttle.neutral_position(cm3, 13)
+    throttle.neutral_position(cm4, 15)
+    """
+
+
+
+cm1.stop()
+cm2.stop()
+cm3.stop()
+cm4.stop()
+print("stop()")
 GPIO.cleanup()                                   
--- src/throttle.py
+++ src/throttle.py
@@ -1,14 +1,29 @@
 import RPi.GPIO as GPIO
 import time
 
+def rotateAngle(motor, angle):
+    duty = angle/18+3
+    GPIO.output(7, True)
+    motor.ChangeDutyCycle(duty)
+    time.sleep(1)
+    GPIO.output(7, False)
+    motor.ChangeDutyCycle(duty)
 
-def throttle(motor):
+
+def throttle(motor, angle=20):
     """Throttle motor to ensure movement"""    
 
     motor.start(0)
     GPIO.output(7, True)
     motor.ChangeDutyCycle(1)
     time.sleep(1)
-    motor.ChangeDutyCycle(2)
-    time.sleep(1)
+    #motor.ChangeDutyCycle(2)
+    #time.sleep(1)
     motor.stop()
+
+def neutral_position(motor, pin_number):
+    motor.start(0)
+    GPIO.output(11, True)
+    motor.ChangeDutyCycle(7)
+    time.sleep(1)
+    
