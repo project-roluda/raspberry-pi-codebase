89949818789a283f5119ae300c406a347b63a9d8
The Floating String <laurencel2001@gmail.com>
2022-01-24 23:24:49 -0500

    Add POST request

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src import movement as mvt
 from threading import Thread
 from src.config import config_obj as config_obj
 
+URL = "http://192.168.2.12:5000"
+
 t1 = Thread(target=mvt.approach())
 t2 = Thread(target=af.sample_audio(3))
 print(config_obj)
@@ -14,4 +16,5 @@ while True:
     if config_obj.movement_halted == True:
         t2.start()
         print(type(config_obj.audio_return_dict)) 
+        requests.post(URL+"/process_audio", json=config_obj.audio_return_dict)
         config_obj.movement_halted = False
