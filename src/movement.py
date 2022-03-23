8b126f2640b16ddf19abc5de483090144efb9674
Laurence Liang <laurencel2001@gmail.com>
2022-03-23 17:23:36 -0400

    Update movement.py

--- src/movement.py
+++ src/movement.py
@@ -6,42 +6,44 @@ import requests
 from src import throttle
 
 
+
+GPIO.setmode(GPIO.BOARD)
+
+# setup GPIO pins for servo motors
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+cm1 = GPIO.PWM(7, 50)
+cm2 = GPIO.PWM(11, 50)
+cm3 = GPIO.PWM(13, 50)
+cm4 = GPIO.PWM(15, 50)
+cm5 = GPIO.PWM(16, 50)
+
+cm2_var = 11
+cm4_var = 11
+
+start = time.time()
+
+cm1.start(0)
+cm2.start(0)
+cm3.start(0)
+cm4.start(0)
+cm5.start(0)
+
+GPIO.output(7, True)
+GPIO.output(11, True)
+GPIO.output(13, True)
+GPIO.output(15, True)
+GPIO.output(16, True)
+
+
+
 def approach():
 
     r = requests.get("https://roluda-test-v6.azurewebsites.net/arm_extension")
-
-    
-    GPIO.setmode(GPIO.BOARD)
-
-    # setup GPIO pins for servo motors
-    GPIO.setup(7, GPIO.OUT)
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.setup(13, GPIO.OUT)
-    GPIO.setup(15, GPIO.OUT)
-    GPIO.setup(16, GPIO.OUT)
-
-    cm1 = GPIO.PWM(7, 50)
-    cm2 = GPIO.PWM(11, 50)
-    cm3 = GPIO.PWM(13, 50)
-    cm4 = GPIO.PWM(15, 50)
-    cm5 = GPIO.PWM(16, 50)
-    
-    cm2_var = 11
-    cm4_var = 11
-
-    start = time.time()
-
-    cm1.start(0)
-    cm2.start(0)
-    cm3.start(0)
-    cm4.start(0)
-    cm5.start(0)
-
-    GPIO.output(7, True)
-    GPIO.output(11, True)
-    GPIO.output(13, True)
-    GPIO.output(15, True)
-    GPIO.output(16, True)
 
     cm1.ChangeDutyCycle(8)
     cm2.ChangeDutyCycle(8)
