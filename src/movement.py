3165b8625a36877d870c85a2a1fee5590668458a
Laurence Liang <laurencel2001@gmail.com>
2022-03-23 16:57:32 -0400

    comment out un-needed code

--- src/movement.py
+++ src/movement.py
@@ -23,7 +23,7 @@ cm5 = GPIO.PWM(16, 50)
 
 def approach():
 
-    r = requests.get("https://roluda-test-1.azurewebsites.net/arm_extension")
+    # r = requests.get("https://roluda-test-1.azurewebsites.net/arm_extension")
 
     cm2_var = 11
     cm4_var = 11
